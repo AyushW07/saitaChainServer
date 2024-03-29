ab69b3e5222609d62e7927fc712051f3f8ec38fd
Lilhorenikita <nikitapalnesto19@gmail.com>
2024-03-29 16:04:29 +0530

    feat:headers put

--- src/index.js
+++ src/index.js
@@ -33,27 +33,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const route = require("./Route/route");
-const mongoose = require("mongoose"); // Removed destructuring from import
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const multer = require("multer");
 
+// Use multer for parsing multipart/form-data
 app.use(multer().any());
 
-// Custom CORS configuration
+// Enable CORS with custom configuration
 const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
-  optionsSuccessStatus: 204, // Usually, 204 is used for preflight response
-  credentials: true, // This allows cookies and other credentials to be sent in cross-origin requests
+  optionsSuccessStatus: 204,
+  credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Origin", "Cookie"],
 };
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions)); // Use the custom CORS options
-
+// Use bodyParser for parsing application/json and application/x-www-form-urlencoded
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
+// Connect to MongoDB
 mongoose.connect(
   "mongodb+srv://vxr73978:H60fu05Yl1so2OiS@cluster0.edp22fy.mongodb.net/Saitachain",
   {
@@ -64,8 +66,21 @@ mongoose.connect(
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err));
 
+// Explicitly set security headers
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader('Content-Security-Policy', "default-src 'self';");
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=()');
+  next();
+});
+
+// Use routes
 app.use("/", route);
 
+// Listen on the specified port
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express app running on port " + (process.env.PORT || 3000));
 });
