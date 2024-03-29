8d4e054c500d179fe12ccc386b62cca1e094c1a6
Lilhorenikita <nikitapalnesto19@gmail.com>
2024-03-29 14:23:43 +0530

    feat:headers

--- src/index.js
+++ src/index.js
@@ -6,9 +6,39 @@ const { Route } = require("express");
 const cors = require("cors");
 const app = express();
 const multer = require("multer");
+const helmet = require("helmet");
 app.use(multer().any());
 // Enable All CORS Requests for development use
 app.use(cors());
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      // Add other directives as needed
+    },
+  })
+);
+
+// Referrer Policy
+app.use(helmet.referrerPolicy({ policy: "no-referrer" }));
+
+// Setting Strict-Transport-Security header
+app.use(helmet.hsts({
+  maxAge: 31536000, // 1 year in seconds
+  includeSubDomains: true, // Apply HSTS policy to all subdomains
+  preload: true
+}));
+
+app.use(helmet.frameguard({
+  action: 'sameorigin'
+}));
+
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'geolocation=(self), microphone=()');
+  next();
+});
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
